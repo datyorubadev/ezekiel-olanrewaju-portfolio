d50d3c8cca07a13945fb8646fb337087f4e7e281
datyorubadev <66226149+datyorubadev@users.noreply.github.com>
2025-12-21 10:37:01 +0100

    Graph commit

--- cms/src/app/(payload)/graphql/route.ts
+++ cms/src/app/(payload)/graphql/route.ts
@@ -1,5 +1,4 @@
 import configPromise from '@payload-config'
-import { GRAPHQL_POST, GRAPHQL_OPTIONS } from '@payloadcms/next/routes'
+import { GRAPHQL_POST } from '@payloadcms/next/routes'
 
 export const POST = GRAPHQL_POST(configPromise)
-export const OPTIONS = GRAPHQL_OPTIONS(configPromise)
